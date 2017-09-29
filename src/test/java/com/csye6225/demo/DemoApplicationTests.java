782a5f974b819c55e940e88e098b7a86b2311a0f
Yashodhan Prabhune <prabhune.y@husky.neu.edu>
2017-09-28 23:54:11 -0400

    Unit Test And Catalina

--- src/test/java/com/csye6225/demo/DemoApplicationTests.java
+++ src/test/java/com/csye6225/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoApplicationTests{
 
   @Ignore
   @Test
