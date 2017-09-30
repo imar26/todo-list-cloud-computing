c837704818d8fb1b7bf4fe7760da83ad0f6d876d
aadeshkranderia <randeria.a@husky.neu.edu>
2017-09-29 21:22:01 -0400

    imports updated

--- src/test/java/com/csye6225/demo/SampleTest.java
+++ src/test/java/com/csye6225/demo/SampleTest.java
@@ -1,15 +1,10 @@
 package com.csye6225.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNull;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SampleTest {
         @Test
         public void findByUserName() throws Exception {
