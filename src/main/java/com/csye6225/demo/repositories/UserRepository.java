37fa93856f191ccfd605a52d2254df4720d745c6
Yashodhan Prabhune <prabhune.y@husky.neu.edu>
2017-09-29 22:07:45 -0400

    Adding team member info in all files

--- src/main/java/com/csye6225/demo/repositories/UserRepository.java
+++ src/main/java/com/csye6225/demo/repositories/UserRepository.java
@@ -1,3 +1,10 @@
+/*
+Yashodhan Prabhune,001220710,prabhune.y@husky.neu.edu
+Bhumika Khatri,001284560,khatri.bh@husky.neu.edu
+Aadesh Randeria,001224139,randeria.a@husky.neu.edu
+Siddhant Chandiwal,001286480,chandiwal.s@husky.neu.edu
+ */
+
 package com.csye6225.demo.repositories;
 
 import com.csye6225.demo.pojo.User;
