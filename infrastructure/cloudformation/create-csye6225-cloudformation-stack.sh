438888cf556f8f589cbd578cb69b24c865d8cd4e
Siddhant Chandiwal <chandiwal.s@husky.neu.edu>
2017-10-05 23:43:17 -0400

    Security Group Name fixed

--- infrastructure/cloudformation/create-csye6225-cloudformation-stack.sh
+++ infrastructure/cloudformation/create-csye6225-cloudformation-stack.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-# Get VPC ID
-export VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[0].VpcId" --output text)
-
-echo $VPC_ID
-
-# Use the VPC ID to get subnet id
-export SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[0].SubnetId" --output text)
-
-echo $SUBNET_ID
-
-aws cloudformation create-stack --stack-name $1 --template-body file://simple-ec2-instance-securitygroup-cloudformation-stack.json --parameters '[{"parameter_key":"SubnetId","paramter_value":$SUBNET_ID}]'S
+aws cloudformation create-stack --stack-name $1 --template-body file://simple-ec2-instance-securitygroup-cloudformation-stack.json
