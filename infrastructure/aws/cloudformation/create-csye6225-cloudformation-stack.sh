ad9e7198843d4aaf6223242bfd75023ee84ac412
Siddhant Chandiwal <chandiwal.s@husky.neu.edu>
2017-10-26 23:34:31 -0400

    Part 2A finish

--- infrastructure/aws/cloudformation/create-csye6225-cloudformation-stack.sh
+++ infrastructure/aws/cloudformation/create-csye6225-cloudformation-stack.sh
@@ -1,3 +1,9 @@
+
+#Yashodhan Prabhune,001220710,prabhune.y@husky.neu.edu
+#Bhumika Khatri,001284560,khatri.bh@husky.neu.edu
+#Aadesh Randeria,001224139,randeria.a@husky.neu.edu
+#Siddhant Chandiwal,001286480,chandiwal.s@husky.neu.edu
+
 #CLOUDFORMATIONSTACK -- stack name
 
 echo "Getting Stack name as input from Terminal"
