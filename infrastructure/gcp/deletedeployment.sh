c45a97950b81ee2aeafada34333e0bbb8761512f
aadeshkranderia <randeria.a@husky.neu.edu>
2017-12-11 19:10:48 -0500

    big ta le

--- infrastructure/gcp/deletedeployment.sh
+++ infrastructure/gcp/deletedeployment.sh
@@ -1,1 +1,4 @@
+cbt deletefamily csyetable csyefamily
+cbt deletetable csyetable
+gcloud beta bigtable instances delete google-cloud-bigtable
 gcloud deployment-manager deployments delete finalpresentation
